3c55765f53018326bb61071c0977d824a789b9dd
HLe4s <asss8593@ajou.ac.kr>
2022-03-09 18:35:10 +0900

    01. freestanding rust binary

---
+++ HLeOs/target/debug/HLeOs.d
@@ -0,0 +1,1 @@
+/home/asss8593/lab/HLeOs/HLeOs/target/debug/HLeOs: /home/asss8593/lab/HLeOs/HLeOs/src/main.rs
---
+++ HLeOs/target/debug/deps/HLeOs-95fb7591c0ea36ee.d
@@ -0,0 +1,5 @@
+/home/asss8593/lab/HLeOs/HLeOs/target/debug/deps/HLeOs-95fb7591c0ea36ee: src/main.rs
+
+/home/asss8593/lab/HLeOs/HLeOs/target/debug/deps/HLeOs-95fb7591c0ea36ee.d: src/main.rs
+
+src/main.rs:
