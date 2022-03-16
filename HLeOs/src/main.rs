2743192472404fc13b33062f4b649a32b49cc7eb
HLe4s <asss8593@ajou.ac.kr>
2022-03-16 21:25:06 +0900

    Chapter2 ended

--- HLeOs/src/main.rs
+++ HLeOs/src/main.rs
@@ -3,10 +3,9 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    static HELLO: &[u8] = b"Hello World!";
     let vga_buffer = 0xb8000 as *mut u8;
 
     for (i, &byte) in HELLO.iter().enumerate() {
@@ -15,8 +14,8 @@ pub extern "C" fn _start() -> ! {
             *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
         }
     }
-
-    loop {}
+    
+    loop{}
 }
 
 /// This function is called on panic.
